0575f6a1a24ac28816c236e2c772fdef7398f46b
Roman Pogorelov <rmk140987@gmail.com>
2020-08-11 14:09:44 +0400

    5.1 Fixed by remarks.

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,11 @@
 let menuButton = document.querySelector(".main-header__button-toggle");
 let menu = document.querySelector(".main-header__nav-menu");
 let nav = document.querySelector(".main-header__nav");
+let mainHeader = document.querySelector(".main-header");
+
 menuButton.addEventListener("click", function () {
   menuButton.classList.toggle("main-header__button-toggle--opened");
   menu.classList.toggle("nav-menu--opened");
   nav.classList.toggle("main-header__nav--full");
+  mainHeader.classList.toggle("main-header--index-page-opened")
 });
