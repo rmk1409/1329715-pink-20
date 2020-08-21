43e50c8dfea8670a90e0604eac33e6bf577292d3
Roman Pogorelov <rmk140987@gmail.com>
2020-08-21 14:49:39 +0300

    8. Finished build task.

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ const rename = require("gulp-rename");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const svgstore = require("gulp-svgstore");
+const del = require("del");
 
 // Styles
 
@@ -24,7 +25,7 @@ const styles = () => {
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
@@ -35,7 +36,7 @@ exports.styles = styles;
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -54,6 +55,7 @@ const images = () => {
       imagemin.mozjpeg({progressive: true}),
       imagemin.svgo()
     ]))
+    .pipe(gulp.dest("build/img"));
 }
 
 exports.images = images;
@@ -62,7 +64,7 @@ exports.images = images;
 const makewebp = () => {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp())
-    .pipe(gulp.dest("source/img"))
+    .pipe(gulp.dest("build/img"));
 }
 exports.makewebp = makewebp;
 
@@ -71,18 +73,53 @@ const sprite = () => {
   return gulp.src("source/img/**/*.svg")
     .pipe(svgstore())
     .pipe(rename("svg-sprite.svg"))
-    .pipe(gulp.dest("source/img"))
+    .pipe(gulp.dest("build/img"));
 }
 
 exports.sprite = sprite;
+
+// Copying to 'build'
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+};
+exports.copy = copy;
+
+const html = () => {
+  return gulp.src([
+    "source/*.html"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+
+};
+exports.html = html;
+
+// Delete 'build'
+const clean = () => {
+  return del("build");
+};
+exports.clean = clean;
+
+const build = gulp.series(clean, copy, html, styles, images, makewebp, sprite);
+exports.build = build;
 
 // Watcher
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+  gulp.watch("source/*.html").on("change", gulp.series(html));
   gulp.watch("source/*.html").on("change", sync.reload);
 }
 
 exports.default = gulp.series(
-  styles, server, watcher
+  build, server, watcher
 );
